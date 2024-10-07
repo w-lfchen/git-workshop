167f7f4b2ac7811f116d19f271e740fede5f1a77
Wölfchen <115360611+W-lfchen@users.noreply.github.com>
2024-10-07 19:24:29 +0200

    init: initial commit

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell { packages = with pkgs; [ marp-cli ]; };
+      formatter.${system} = pkgs.nixfmt-rfc-style;
+    };
+}
